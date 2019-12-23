195865d5b5854e50516a41cec8281c584d139583
Marko <markogajiin@gmail.com>
2019-12-23 23:25:49 +0100

    fix

--- weather_api/routes/weather.js
+++ weather_api/routes/weather.js
@@ -5,7 +5,6 @@ const { fetchWeather, getWeather } = require('../controllers/weather');
 const router = exporess.Router();
 
 router.route('/update').get(fetchWeather);
-// router.route('/').get(getAll);
 router.route('/:city').get(getWeather);
 
 module.exports = router;

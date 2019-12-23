ef2a906bd5723a527ad4a94382dd706cf20a449a
Marko <markogajiin@gmail.com>
2019-12-23 20:39:23 +0100

    User model setup

---
+++ authentication/models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        minlength: 5
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        match: [
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Email is not valid'
+        ]
+    },
+    password: {
+        type: String,
+        reuiqred: [true, 'Password is required'],
+        minlength: 4,
+        select: false
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);

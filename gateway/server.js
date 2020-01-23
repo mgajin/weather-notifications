6e5979725b89434367ed06cb6d9e81265cad4256
Marko <markogajiin@gmail.com>
2020-01-23 23:01:47 +0100

    fix

--- gateway/server.js
+++ gateway/server.js
@@ -14,11 +14,23 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+// CORS middleware
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header('access-Control-Allow-Methods', 'GET, POST, DELETE');
+    next();
+});
+
 const PORT = process.env.PORT || 3000;
 
 const server = gateway({
     routes: [
-        { prefix: '/user', target: 'http://localhost:3001/v1/' },
+        { prefix: '/auth', target: 'http://localhost:3001/v1/auth/' },
+        { prefix: '/service', target: 'http://localhost:3001/v1/service/' },
         { prefix: '/weather', target: 'http://localhost:3002/v1/' }
     ]
 });

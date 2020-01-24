3c3f222034e0178178328b3ffea54771b3f79d64
Marko <markogajiin@gmail.com>
2020-01-24 17:01:44 +0100

    fix

--- authentication/controllers/users.js
+++ authentication/controllers/users.js
@@ -5,17 +5,30 @@ const User = require('../models/User');
 // @route   GET /users?subscribed=true
 exports.getUsers = async (req, res) => {
     let users;
-    const query = req.query.subscribed;
+    // const query = req.query.subscribed;
     // If query than return subscribed users
-    if (query) {
-        users = await User.find({ subscribed: query });
-    } else {
-        users = await User.find();
+    // if (query) {
+    //     users = await User.find({
+    //         subscription: {
+    //             status: true
+    //         }
+    //     });
+    // } else {
+    //     users = await User.find();
+    // }
+    users = await User.find();
+    let subscribed_users = new Array();
+
+    for (i in users) {
+        let user = users[i];
+        if (user.subscription.status) {
+            subscribed_users.push(user);
+        }
     }
 
-    if (!users.length) {
+    if (!subscribed_users.length) {
         return res.status(404).send('Users not found');
     }
 
-    res.status(200).json({ success: true, users });
+    res.status(200).json({ success: true, subscribed_users });
 };
